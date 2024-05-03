3f0cd895f31d67d0c58d4388127f76ac2b92299e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 19:54:09 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int digitado;
+
+Console.WriteLine("--- Numeros negativos ---");
+
+Console.Write("Digite um numero: ");
+digitado = Convert.ToInt32(Console.ReadLine());
+
+if (digitado < 0)
+{
+   Console.ForegroundColor = ConsoleColor.Green;
+   Console.WriteLine("Você digitou um numero negativo");
+   Console.ResetColor();
+}
+
+
